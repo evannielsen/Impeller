4bf03c15bd6e26cbe596fcd5e25cdb112af73ae3
Evan Nielsen <evan@housenielsen.com>
2017-11-20 20:29:43 -0500

    added comment block

--- Impeller.Extensions/Dictionaries/DictionaryExtensions.cs
+++ Impeller.Extensions/Dictionaries/DictionaryExtensions.cs
@@ -6,6 +6,14 @@ namespace Impeller.Extensions.Dictionaries
 {
     public static class DictionaryExtensions
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="TKey"></typeparam>
+        /// <typeparam name="TValue"></typeparam>
+        /// <param name="dictionary"></param>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
         public static void AddOrReplace<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, TValue value)
         {
             if (dictionary == null)
