389168c75727c6c5843f53ba737ce4a45d2027f7
Evan Nielsen <enielsen@rightpoint.com>
2017-11-20 20:12:56 -0500

    reorganized helpers

---
+++ Impeller.Helpers/Enums/EnumHelper.cs
@@ -0,0 +1,22 @@
+﻿using Impeller.Extensions.Enums;
+using Impeller.Models.Selections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Impeller.Helpers.Enums
+{
+    public static class EnumHelper
+    {
+        public static IEnumerable<Enum> GetElements<T>() where T : struct, IConvertible
+        {
+            return Enum.GetValues(typeof(T)).Cast<Enum>();
+        }
+
+        public static IEnumerable<SelectListItem> GetSelectListItems<T>() where T : struct, IConvertible
+        {
+            return GetElements<T>().Select(e => new SelectListItem { Display = e.DisplayName(), Value = e.Value().ToString() });
+        }
+    }
+}
---
+++ Impeller.Helpers/Random/RandomStringGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Impeller.Helpers.Random
+{
+    public static class RandomStringGenerator
+    {
+        private static readonly System.Random random = new System.Random();
+
+        private const string availableChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+        public static string GenerateString(int length)
+        {
+            return new string(
+                Enumerable.Repeat(availableChars, length)
+                .Select(s => s[random.Next(s.Length)])
+                .ToArray()
+                );
+        }
+
+    }
+}
